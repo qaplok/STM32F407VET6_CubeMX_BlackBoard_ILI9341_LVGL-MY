a2363bf314d67f8b27d070a442203f4ad108b392
Qaplok <Qaplok@noreply.gitcode.com>
2025-01-03 10:03:50 +0500

    2.3028kHz on PA0

--- Src/tim.c
+++ Src/tim.c
@@ -40,11 +40,11 @@ void MX_TIM2_Init(void)
 
   /* USER CODE BEGIN TIM2_Init 1 */
 
-  /* USER CODE END TIM2_Init 1 */
+  /* USER CODE END TIM2_Init 1 2.3028kHz*/
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 73;
+  htim2.Init.Prescaler = 569;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 999;
+  htim2.Init.Period = 63;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
@@ -67,7 +67,7 @@ void MX_TIM2_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 500;
+  sConfigOC.Pulse = 32;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
